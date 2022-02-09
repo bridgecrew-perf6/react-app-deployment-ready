10ee64095a131e537d04d64cfe8b563696a2a0fb
Sintu Singh <sintu.singh@Sintus-MacBook-Pro.local>
2022-02-08 22:02:45 -0500

    React app deploy ready

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+function App() {
+
+  return (
+    <div className="app">
+      <main>
+        Hello! App is Running...
+      </main>
+    </div>
+  );
+}
+
+export default App;
